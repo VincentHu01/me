fbda5ccc614feabd8c1e35ae7be0608c676f3822
VincentHu01 <46297740+VincentHu01@users.noreply.github.com>
2023-01-16 14:41:20 +0800

    m

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/me/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230116.143651';
+const cacheName = 'chirpy-20230116.144049';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
