b930f3a3c852d46658e625b5723fdbcd2bbe63c0
VincentHu01 <2272714210@qq.com>
2023-02-20 14:17:21 +0800

    update

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/me/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230220.141558';
+const cacheName = 'chirpy-20230220.141704';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
